d597bbb6d7840d8c4d3513670101e1d4acdff4cd
given chow <jiwenzhou@tencent.com>
2022-10-18 17:27:31 +0800

    feat:异步action完成

---
+++ src/03应用(基于脚手架)/first_demo/04react-redux/04redux异步action/src/redux/countReducer.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {*} preState 之前的状态
+ * @param {*} action 动作对象
+ */
+import { INCREMENT, DECREMENT } from './constant';
+export default function countReducer(preState = 0, action) {
+  const { type, data } = action;
+  switch (type) {
+    case INCREMENT:
+      return preState + data;
+    case DECREMENT:
+      return preState - data;
+    default:
+      return preState;
+  }
+}
