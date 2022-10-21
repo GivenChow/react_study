18e82d7e27eeccb69d11643c7d8d983bb4457227
given chow <jiwenzhou@tencent.com>
2022-10-21 10:58:55 +0800

    feat: Provider完成

--- src/03应用(基于脚手架)/first_demo/src/index.jsx
+++ src/03应用(基于脚手架)/first_demo/src/index.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import store from './redux/store';
+import { Provider } from 'react-redux';
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-store.subscribe(() => {
-  root.render(<App />);
-});
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+// store.subscribe(() => {
+//   root.render(<App />);
+// });
 // ReactDOM.render(<App />, document.getElementById('root'))
